bdb11fd6935ac62c63b13e0b390834e6eb3bafa3
BRIAN_AVILA <160604984+brian-j-avila@users.noreply.github.com>
2024-05-20 11:33:37 -0500

    ajustes de ñps archivos de admin

--- RH/empleados_up.php
+++ RH/empleados_up.php
@@ -58,7 +58,7 @@ if(isset($_POST["update"]))
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <title>Actualizar datos</title>
+    <title>Actualizar datos empleados</title>
 </head>
 
 <body onload="centrar();">
