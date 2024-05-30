606c04b71115126f99cdeee8aea713fc246aee94
BRIAN_AVILA <160604984+brian-j-avila@users.noreply.github.com>
2024-05-30 08:41:30 -0500

    ajuste a la encriptacion de coontraseña

--- RH/index.php
+++ RH/index.php
@@ -1,223 +1,127 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "nomina_algj";
+
+// Inicializar las variables de búsqueda
+$search_term = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Asignar valor de búsqueda si está disponible
+    $search_term = isset($_POST["search_term"]) ? $_POST["search_term"] : "";
+}
+
+try {
+    $conexion = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexion->exec("SET CHARACTER SET utf8");
+
+    // Consulta SQL con filtro de búsqueda
+    $sql = "SELECT usuarios.id_us, usuarios.nombre_us, usuarios.apellido_us, usuarios.correo_us, usuarios.tel_us, usuarios.foto, roles.tp_user, puestos.cargo, puestos.salario 
+            FROM usuarios 
+            LEFT JOIN roles ON usuarios.id_rol = roles.id 
+            LEFT JOIN puestos ON usuarios.id_puesto = puestos.id 
+            WHERE usuarios.id_rol >= 6";
+
+    // Aplicar filtro si se proporciona un término de búsqueda
+    if (!empty($search_term)) {
+        $sql .= " AND (usuarios.nombre_us LIKE '%$search_term%' OR usuarios.apellido_us LIKE '%$search_term%' OR usuarios.tel_us LIKE '%$search_term%' OR usuarios.id_us LIKE '%$search_term%' OR roles.tp_user LIKE '%$search_term%' OR puestos.cargo LIKE '%$search_term%')";
+    }
+
+    $stmt = $conexion->prepare($sql);
+    $stmt->execute();
+    $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "Error de conexión a la base de datos: " . $e->getMessage();
+    exit();
+}
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
-    <title>TABLAS ADMIN</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administrador</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <style>
+        .card {
+            margin-bottom: 20px;
+        }
+
+        .card-img-top {
+            width: 100%;
+            height: 200px;
+            /* Define una altura fija */
+            object-fit: cover;
+            /* Ajusta la imagen para cubrir el área definida */
+        }
+    </style>
 </head>
+
 <body>
-    
-<div class="content">
+    <?php include '../admin/PHP/nav.php'; ?>
+
     <div class="container">
-        <h2 class="mb-5">TABLA ESTADOS</h2>
-        <div class="table-responsive">
-            <table class="table table-striped custom-table">
-            <div class="button"><a href='PHP/crear_php/estados.php'>CARGAR ESTADOS</a></div>  
-                <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">ESTADO</th>
-                    <th scope="col">Acciones</th>
-                    <th scope="col">CARGAR</th>
-                </tr>
-                </thead>
-                <tbody class="tbody">
-                <?php
-                include 'conexion/db.php';
+        <div class="container mt-5">
+            <h2 class="mb-4">Trabajadores</h2>
+            <div class="row mb-4">
+                <div class="col">
+                    <form method="post" class="form-inline">
+                        <div class="form-group mr-2">
+                            <input type="text" class="form-control" name="search_term" placeholder="Buscar...">
+                        </div>
+                        <button type="submit" class="btn btn-primary mr-2"><i class="fas fa-search"></i> Buscar</button>
+                        <?php if (!empty($search_term)) : ?>
+                            <a href="." class="btn btn-secondary"><i class="fas fa-times"></i> Limpiar</a>
+                        <?php endif; ?>
+                    </form>
+                </div>
+            </div>
+            <div class="row">
+                <?php if (isset($usuarios) && !empty($usuarios)) : ?>
+                    <?php foreach ($usuarios as $usuario) : ?>
+                        <div class="col-md-4">
+                            <div class="card">
+                                <?php if (!empty($usuario['foto'])) : ?>
+                                    <img class="card-img-top" src="data:image/jpeg;base64,<?php echo base64_encode($usuario['foto']); ?>" alt="Foto">
+                                <?php endif; ?>
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php echo $usuario['nombre_us'] . ' ' . $usuario['apellido_us']; ?></h5>
+                                    <p class="card-text"><strong>Cédula:</strong> <?php echo $usuario['id_us']; ?></p>
+                                    <p class="card-text"><strong>Rol:</strong> <?php echo $usuario['tp_user']; ?></p>
+                                    <p class="card-text"><strong>Cargo:</strong> <?php echo $usuario['cargo']; ?></p>
+                                    <p class="card-text"><strong>Salario:</strong> <?php echo $usuario['salario']; ?></p>
+                                    <!-- Formulario oculto para enviar el id_us -->
+                                    <form action="/liquidacion/liquidar.php" method="POST" style="display: none;" id="liquidar_form_<?php echo $usuario['id_us']; ?>">
+                                        <input type="hidden" name="id_us" value="<?php echo $usuario['id_us']; ?>">
+                                    </form>
+                                    <!-- Botón para liquidar -->
+                                    <button class="btn btn-success btn-sm" onclick="liquidar(<?php echo $usuario['id_us']; ?>)">liquidar</button>
+                                </div>
+                            </div>
+                        </div>
 
-                $sql = "SELECT * FROM estado";
-                $result = $conn->query($sql);
-
-                if ($result->num_rows > 0) {
-                    while ($row = $result->fetch_assoc()) {
-                        echo "<tr>";
-                        echo "<td>" . $row['ID_Es'] . "</td>";
-                        echo "<td>" . $row['Estado'] . "</td>";
-                        echo "<td><a href='PHP/editar_php/editar_estados.php?id=" . $row['ID_Es'] . "'>Editar</a> | <a href='PHP/eliminar_php/eliminar_estado.php?id=" . $row['ID_Es'] . "'>Eliminar</a></td>";
-                        echo "<td><a href='PHP/crear_php/estados.php'>CARGAR</a></td>";
-                        echo "</tr>";
-                    }
-                } else {
-                    echo "<tr><td colspan='4'>No hay estados registrados.</td></tr>";
-                }
-                ?>
-                </tbody>
-            </table>
-            <!-- Botón CARGAR siempre activo -->
-          
+                    <?php endforeach; ?>
+                <?php else : ?>
+                    <div class="col">
+                        <div class="alert alert-warning" role="alert">
+                            No se encontraron trabajadores.
+                        </div>
+                    </div>
+                <?php endif; ?>
+            </div>
         </div>
     </div>
-</div>
-<div class="content">
-    <div class="container">
-        <h2 class="mb-5">TABLA ROLES</h2>
-        <div class="table-responsive">
-            <table class="table table-striped custom-table">
-            <div class="button"><a href='PHP/crear_php/roles.php'>CARGAR ROLES</a></div>  
-                <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">ROL</th>
-                    <th scope="col">Acciones</th>
-                    <th scope="col">CARGAR</th>
-                </tr>
-                </thead>
-                <tbody class="tbody">
-                <?php
-                include 'conexion/db.php';
+    <script>
+    function liquidar(id) {
+        document.getElementById('liquidar_form_' + id).submit();
+    }
+</script>
 
-                $sql = "SELECT * FROM roles";
-                $result = $conn->query($sql);
-
-                if ($result->num_rows > 0) {
-                    while ($row = $result->fetch_assoc()) {
-                        echo "<tr>";
-                        echo "<td>" . $row['ID'] . "</td>";
-                        echo "<td>" . $row['TP_user'] . "</td>";
-                        echo "<td><a href='PHP/editar_php/editar_roles.php?id=" . $row['ID'] . "'>Editar</a> | <a href='PHP/eliminar_php/eliminar_rol.php?id=" . $row['ID'] . "'>Eliminar</a></td>";
-                        echo "<td><a href='PHP/crear_php/roles.php'>CARGAR</a></td>";
-                        echo "</tr>";
-                    }
-                } else {
-                    echo "<tr><td colspan='4'>No hay ROLES registrados.</td></tr>";
-                }
-                ?>
-                </tbody>
-            </table>
-            <!-- Botón CARGAR siempre activo -->
-          
-        </div>
-    </div>
-</div>
-<div class="content">
-    <div class="container">
-        <h2 class="mb-5">TABLA SALUD</h2>
-        <div class="table-responsive">
-            <table class="table table-striped custom-table">
-            <div class="button"><a href='PHP/crear_php/salud.php'>CARGAR SALUD</a></div>  
-                <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">VALOR</th>
-                    <th scope="col">Acciones</th>
-                    <th scope="col">CARGAR</th>
-                </tr>
-                </thead>
-                <tbody class="tbody">
-                <?php
-                include 'conexion/db.php';
-
-                $sql = "SELECT * FROM salud";
-                $result = $conn->query($sql);
-
-                if ($result->num_rows > 0) {
-                    while ($row = $result->fetch_assoc()) {
-                        echo "<tr>";
-                        echo "<td>" . $row['ID'] . "</td>";
-                        echo "<td>" . $row['valor'] . " %"."</td>";
-                        echo "<td><a href='PHP/editar_php/editar_salud.php?id=" . $row['ID'] . "'>Editar</a> | <a href='PHP/eliminar_php/eliminar_salud.php?id=" . $row['ID'] . "'>Eliminar</a></td>";
-                        echo "<td><a href='PHP/crear_php/salud.php'>CARGAR</a></td>";
-                        echo "</tr>";
-                    }
-                } else {
-                    echo "<tr><td colspan='4'>No hay SALUD registrados.</td></tr>";
-                }
-                ?>
-                </tbody>
-            </table>
-            
-          
-        </div>
-    </div>
-</div>
-<div class="content">
-    <div class="container">
-        <h2 class="mb-5">TABLA PENSION</h2>
-        <div class="table-responsive">
-            <table class="table table-striped custom-table">
-            <div class="button"><a href='PHP/crear_php/pension.php'>CARGAR PENSION</a></div>  
-                <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">VALOR</th>
-                    <th scope="col">Acciones</th>
-                    <th scope="col">CARGAR</th>
-                </tr>
-                </thead>
-                <tbody class="tbody">
-                <?php
-                include 'conexion/db.php';
-
-                $sql = "SELECT * FROM pension";
-                $result = $conn->query($sql);
-
-                if ($result->num_rows > 0) {
-                    while ($row = $result->fetch_assoc()) {
-                        echo "<tr>";
-                        echo "<td>" . $row['ID'] . "</td>";
-                        echo "<td>" . $row['valor'] . " %"."</td>";
-                        echo "<td><a href='PHP/editar_php/editar_pension.php?id=" . $row['ID'] . "'>Editar</a> | <a href='PHP/eliminar_php/eliminar_pension.php?id=" . $row['ID'] . "'>Eliminar</a></td>";
-                        echo "<td><a href='PHP/crear_php/pension.php'>CARGAR</a></td>";
-                        echo "</tr>";
-                    }
-                } else {
-                    echo "<tr><td colspan='4'>No hay pension registrados.</td></tr>";
-                }
-                ?>
-                </tbody>
-            </table>
-            
-          
-        </div>
-    </div>
-</div>
-<div class="content">
-    <div class="container">
-        <h2 class="mb-5">TABLA VALOR HORAS EXTRA</h2>
-        <div class="table-responsive">
-            <table class="table table-striped custom-table">
-            <div class="button"><a href='PHP/crear_php/vhe.php'>CARGAR VALOR</a></div>  
-                <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">VALOR</th>
-                    <th scope="col">Acciones</th>
-                    <th scope="col">CARGAR</th>
-                </tr>
-                </thead>
-                <tbody class="tbody">
-                <?php
-                include 'conexion/db.php';
-
-                $sql = "SELECT * FROM v_h_extra";
-                $result = $conn->query($sql);
-
-                if ($result->num_rows > 0) {
-                    while ($row = $result->fetch_assoc()) {
-                        echo "<tr>";
-                        echo "<td>" . $row['ID'] . "</td>";
-                        echo "<td>" . $row['V_H_extra'] ."</td>";
-                        echo "<td><a href='PHP/editar_php/editar_valor_extra.php?id=" . $row['ID'] . "'>Editar</a> | <a href='PHP/eliminar_php/eliminar_v_hora_extra.php?id=" . $row['ID'] . "'>Eliminar</a></td>";
-                        echo "<td><a href='PHP/crear_php/vhe.php'>CARGAR</a></td>";
-                        echo "</tr>";
-                    }
-                } else {
-                    echo "<tr><td colspan='4'>No hay valores de hora extra registrados.</td></tr>";
-                }
-                ?>
-                </tbody>
-            </table>
-            
-          
-        </div>
-    </div>
-</div>
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/main.js"></script>
 </body>
+
 </html>
